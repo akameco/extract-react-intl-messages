c5443d0d91ccd2977344c106cdde5cc871f4fd49
akameco <akameco.t@gmail.com>
2017-05-17 09:57:27 +0900

    Update

--- test/fixtures/components/App/messages.js
+++ test/fixtures/components/App/messages.js
@@ -2,12 +2,6 @@
 import { defineMessages } from 'react-intl'
 
 export default defineMessages({
-  hello: {
-		id: 'components/App/hello',
-    defaultMessage: 'hello',
-  },
-  world: {
-		id: 'components/App/world',
-    defaultMessage: 'world',
-  },
+  hello: 'hello',
+  world: 'world'
 })
