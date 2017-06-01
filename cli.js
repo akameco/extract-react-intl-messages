37912b9dea974ac0c874a21e669c2380a9d31fdd
akameco <akameco.t@gmail.com>
2017-06-01 09:56:21 +0900

    Add cli.js

---
+++ cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+'use strict'
+const meow = require('meow')
+const fn = require('.')
+
+const cli = meow(
+  `
+  Usage
+  $ extract-react-intl-messages <input>
+  $ extract-messages <input>
+
+  Options
+  -o, --output       Output directory [require: true]
+  -l, --locales      locales [require: true]
+  -f, --format       json|yaml [default: json]
+  --default-locale   default locale
+
+  Example
+  $ extract-messages --locales=ja,en --output app/translations 'app/**/*.js'
+  $ extract-messages -l=ja,en -o app/translations -f yaml 'app/**/messages.js'
+`,
+  {
+    strings: {
+      output: true,
+      locales: true,
+      format: true
+    },
+    alias: {
+      o: 'output',
+      l: 'locales',
+      f: 'format'
+    },
+    default: {
+      format: 'json'
+    }
+  }
+)
+
+const { output, locales } = cli.flags
+
+if (!output) {
+  console.log('ERROR: required output')
+  process.exit(1)
+}
+
+if (!locales || typeof locales !== 'string') {
+  console.log('ERROR: required locales')
+  process.exit(1)
+}
+
+const localesMap = locales.split(',')
+
+fn(localesMap, cli.input[0], output, cli.flags)
