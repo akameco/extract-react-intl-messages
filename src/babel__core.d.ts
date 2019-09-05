9265d73a86d0a47a8a24b9e69799aa3b61e3fe4c
akameco <akameco.t@gmail.com>
2019-09-06 01:14:15 +0900

    chore(deps): upgrade deps

--- src/babel__core.d.ts
+++
@@ -1,6 +0,0 @@
-import babel from '@babel/core'
-
-declare module '@babel/core' {
-  function resolvePlugin(name: string, dirname: string): string | null
-  function resolvePreset(name: string, dirname: string): string | null
-}
