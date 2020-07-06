1f1397febe947161ef59fddead17abc2d25424a7
Tjelvar Olsen <tenestoro@googlemail.com>
2020-07-06 11:41:22 +0200

    added indent option

--- src/cli.ts
+++ src/cli.ts
@@ -63,7 +63,11 @@ const cli = meow(
       extractFromFormatMessageCall: {
         type: 'boolean',
         default: true
-      }
+      },
+      indent: {
+        type: 'number',
+        default: 2
+      },
     }
   }
 )
